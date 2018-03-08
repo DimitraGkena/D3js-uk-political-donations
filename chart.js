c534bef187b2e68f9e410b509d4a1bedef4b9956
DimitraGkena <p15gken@ionio.gr>
2018-03-08 21:09:26 +0200

    Update chart.js

--- chart.js
+++ chart.js
@@ -21,7 +21,7 @@ var entityCentres = {
 		individual: {x: w / 3.65, y: h / 3.3},
 	};
 //p1: allazw ta xrwmata
-var fill = d3.scale.ordinal().range(["#A52A2A", "#A9A9A9", "#20B2AA"]);
+var fill = d3.scale.ordinal().range(["#A52A2A", "#A9A9A9", "#87CEFA"]);
 
 var svgCentre = { 
     x: w / 3.6, y: h / 2
