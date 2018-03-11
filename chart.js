57408a2b4038de41a3222013b10704f09dfade16
DimitraGkena <p15gken@ionio.gr>
2018-03-11 18:26:42 +0200

    Update chart.js

--- chart.js
+++ chart.js
@@ -4,7 +4,7 @@ var padding = 2;
 var nodes = [];
 var force, node, data, maxVal;
 var brake = 0.2;
-var sound = new Audio("Click sound effect.mp3"); // vazw ixo 
+var sound = new Audio("sound.mp3"); // vazw ixo 
 var radius = d3.scale.sqrt().range([10, 20]);
 
 var partyCentres = { 
@@ -219,21 +219,30 @@ function all(e) {
 
 function moveToAmount(alpha) {
 	return function(d) {
-		if (d.value <= 50000) { 
-			centreX = svgCentre.x ;
-			centreY = svgCentre.y -50;
-		} else if (d.value <= 350000) { 
-			centreX = svgCentre.x + 150;
-			centreY = svgCentre.y ;
-		} else if (d.value <= 20000000){ 
-			centreX = svgCentre.x + 300;
-			centreY = svgCentre.y + 50;
+		var centreX;
+		var centreY;
+		if (d.value <= 50000){
+			centreX = svgCentre.x +70;
+			centreY = svgCentre.y -70;
+		} else if (d.value <= 500000){
+			centreX = svgCentre.x +450;
+			centreY = svgCentre.y -70;
+		} else if (d.value <= 1000000){
+			centreX = svgCentre.x +70;
+			centreY = svgCentre.y +250;
+		} else if (d.value <= 5000000){
+			centreX = svgCentre.x +550;
+			centreY = svgCentre.y +250;
+		} else {
+			centreX = svgCentre.x +650;
+			centreY = svgCentre.y +300;
 		}
-
+		
 		d.x += (centreX - d.x) * (brake + 0.02) * alpha * 1.1;
 		d.y += (centreY - d.y) * (brake + 0.02) * alpha * 1.1;
 	};
 }
+
 
 
 function moveToCentre(alpha) {
