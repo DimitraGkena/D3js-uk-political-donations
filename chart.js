a23e7ef39e2076afeb0b16be562faf72f312ca17
DimitraGkena <p15gken@ionio.gr>
2018-03-11 18:37:57 +0200

    Update chart.js

--- chart.js
+++ chart.js
@@ -225,17 +225,17 @@ function moveToAmount(alpha) {
 			centreX = svgCentre.x +70;
 			centreY = svgCentre.y -70;
 		} else if (d.value <= 500000){
-			centreX = svgCentre.x +450;
+			centreX = svgCentre.x +350;
 			centreY = svgCentre.y -70;
 		} else if (d.value <= 1000000){
 			centreX = svgCentre.x +70;
 			centreY = svgCentre.y +250;
 		} else if (d.value <= 5000000){
+			centreX = svgCentre.x +450;
+			centreY = svgCentre.y +150;
+		} else {
 			centreX = svgCentre.x +550;
 			centreY = svgCentre.y +250;
-		} else {
-			centreX = svgCentre.x +650;
-			centreY = svgCentre.y +300;
 		}
 		
 		d.x += (centreX - d.x) * (brake + 0.02) * alpha * 1.1;
