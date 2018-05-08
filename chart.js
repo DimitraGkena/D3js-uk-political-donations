d5e99e82c916ff6394fe671c47d6169ba7dc2591
DimitraGkena <p15gken@ionio.gr>
2018-05-08 10:21:20 +0300

    Update chart.js

--- chart.js
+++ chart.js
@@ -347,7 +347,7 @@ function mouseover(d, i) {
     .style("top", (parseInt(d3.select(this).attr("cy") - (d.radius+150)) + offset.top) + "px")
 		.html(infoBox)
 			.style("display","block");
-	
+	appendHistory(imageFile)
 	
 	
 	}
